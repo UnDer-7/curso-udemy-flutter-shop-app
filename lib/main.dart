9b2c79627174cadd4613994b0d691499b79b2847
UnDer-7 <mateus7532@gmail.com>
2019-07-07 17:08:14 -0300

    class 189 - Working with Providers & Listeners

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import './screens/products_overview_screen.dart';
 import './routes.dart';
-
+import './providers/products.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
     @override
     Widget build(BuildContext context) {
-        return MaterialApp(
-            title: 'MyShop',
-            theme: ThemeData(
-                primarySwatch: Colors.purple,
-                accentColor: Colors.orangeAccent,
-                fontFamily: 'Lato',
+        return ChangeNotifierProvider(
+            builder: (BuildContext ctx) => Products(),
+            child: MaterialApp(
+                title: 'MyShop',
+                theme: ThemeData(
+                    primarySwatch: Colors.purple,
+                    accentColor: Colors.orangeAccent,
+                    fontFamily: 'Lato',
+                ),
+                home: ProductsOverviewScreen(),
+                routes: Routes.availableRoutes,
             ),
-            home: ProductsOverviewScreen(),
-            routes: Routes.availableRoutes,
         );
     }
 }
---
+++ lib/widgets/products_grid.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import './product_item.dart';
+import '../providers/products.dart';
+
+class ProductsGrid extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+        final productData = Provider.of<Products>(context);
+        final products = productData.items;
+
+        return GridView.builder(
+            padding: const EdgeInsets.all(10),
+            itemCount: products.length,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 3 / 2,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+            ),
+            itemBuilder: (BuildContext ctx, int i) =>
+                ProductItem(
+                    products[i].id,
+                    products[i].title,
+                    products[i].imageUrl,
+                ),
+        );
+    }
+}
