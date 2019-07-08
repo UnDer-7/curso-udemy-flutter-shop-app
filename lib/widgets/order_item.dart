4d86e03f63d8cc4112d21dc919cbc84b4a447064
UnDer-7 <mateus7532@gmail.com>
2019-07-07 22:46:34 -0300

    class 208 - Making Orders Expandable & Stateful Widgets vs Providers

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -1,12 +1,21 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import '../providers/orders.dart' as ord;
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
     final ord.OrderItem order;
 
     OrderItem(this.order);
+
+    @override
+    _OrderItemState createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+    bool _expanded = false;
 
     @override
     Widget build(BuildContext context) {
@@ -15,17 +24,56 @@ class OrderItem extends StatelessWidget {
             child: Column(
                 children: <Widget>[
                     ListTile(
-                        title: Text('\$${order.amount}'),
-                        subtitle: Text(DateFormat('dd/MM/yyyy | hh:mm').format(order.dateTime)),
+                        title: Text('\$${widget.order.amount.toStringAsFixed(2)}'),
+                        subtitle: Text(DateFormat('dd/MM/yyyy | hh:mm').format(widget.order.dateTime)),
                         trailing: IconButton(
-                            onPressed: () {},
+                            onPressed: () {
+                                setState(() {
+                                    _expanded = !_expanded;
+                                });
+                            },
                             icon: Icon(
-                                Icons.expand_more,
+                                _getIcon,
                             ),
                         ),
-                    )
+                    ),
+                    if (_expanded) Container(
+                        padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+                        height: min(
+                            widget.order.products.length * 20.0 + 10.0,
+                            180,
+                        ),
+                        child: ListView(
+                            children: widget.order.products
+                                .map(
+                                    (prod) => Row(
+                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                        children: <Widget>[
+                                            Text(
+                                                prod.title,
+                                                style: TextStyle(
+                                                    fontSize: 18,
+                                                    fontWeight: FontWeight.bold,
+                                                ),
+                                            ),
+                                            Text(
+                                                '${prod.quantity}x \$${prod.price}',
+                                                style: TextStyle(
+                                                    fontSize: 18,
+                                                    color: Colors.grey,
+                                                ),
+                                            ),
+                                        ],
+                                    )).toList(),
+                        ),
+                    ),
                 ],
             ),
         );
+    }
+
+    IconData get _getIcon {
+        if (_expanded) return Icons.expand_less;
+        return Icons.expand_more;
     }
 }
